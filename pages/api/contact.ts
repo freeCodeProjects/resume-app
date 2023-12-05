aa8dddb9cc8999a5a0652e5a6ea62dd786c40762
Akash187 <kumarakashseth007@gmail.com>
2023-12-05 17:26:31 +0530

    feat: hooked contact to send real mail

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Client } from '@notionhq/client'
 
 type ContactData = {
 	name: string
@@ -18,12 +17,38 @@ export default async function handler(
 	switch (method) {
 		case 'POST':
 			try {
-				const response = await newEntryToDatabase(
-					body.name,
-					body.email,
-					body.subject,
-					body.message
-				)
+				const data = {
+					sender: {
+						name: body.name,
+						email: body.email
+					},
+					to: [
+						{
+							email: 'andrew.white@freecodeproject.org',
+							name: 'Andrew White'
+						}
+					],
+					subject: body.subject,
+					htmlContent: `
+					<html>
+						<head></head>
+						<body>
+							<p style="white-space: pre-wrap;">${body.message}</p>
+						</body>
+					</html>`
+				}
+
+				const headers = {
+					accept: 'application/json',
+					'Content-Type': 'application/json',
+					'api-key': process.env.BREVO_API_KEY as string
+				}
+
+				const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+					method: 'POST',
+					body: JSON.stringify(data),
+					headers
+				})
 				return res.status(200).send(response)
 			} catch (error) {
 				// console.log(error)
@@ -33,51 +58,4 @@ export default async function handler(
 			res.setHeader('Allow', ['POST'])
 			return res.status(405).end(`Method ${method} Not Allowed`)
 	}
-}
-
-const newEntryToDatabase = async function (
-	name: string,
-	email: string,
-	subject: string,
-	message: string
-) {
-	const notion = new Client({ auth: process.env.NOTION_KEY })
-
-	return notion.pages.create({
-		parent: {
-			database_id: process.env.CONTACT_DB as string
-		},
-		properties: {
-			Name: {
-				title: [
-					{
-						text: {
-							content: name
-						}
-					}
-				]
-			},
-			Email: {
-				email
-			},
-			Subject: {
-				rich_text: [
-					{
-						text: {
-							content: subject
-						}
-					}
-				]
-			},
-			Message: {
-				rich_text: [
-					{
-						text: {
-							content: message
-						}
-					}
-				]
-			}
-		}
-	})
 }
